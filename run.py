c7c1a5c30d1cfa96880cc109ebe3f848a90ea2a5
Kaushal Kumar Ray <raykaushal456@gmail.com>
2026-03-12 22:03:42 +0530

    Relod Issue resolved

--- run.py
+++ run.py
@@ -2,5 +2,10 @@ from backend.app import create_app
 
 app = create_app()
 
+@app.route("/health")
+def health():
+    return "Status: All Servers are healthy and running smoothly!"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
